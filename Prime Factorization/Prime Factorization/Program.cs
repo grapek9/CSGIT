e360685570fe92467253759551d9ea657d517f5e
Grapek <zerex93@gmail.com>
2015-04-01 14:46:58 +0200

    Prime Factorization Project

--- Prime Factorization/Prime Factorization/Program.cs
+++ Prime Factorization/Prime Factorization/Program.cs
@@ -10,6 +10,16 @@ namespace Prime_Factorization
     {
         static void Main(string[] args)
         {
+            String input;
+            int number;
+            Console.WriteLine("Welcome To Prime Factorization Program");
+            Console.WriteLine("Program Will Display Prime Numbers until reach number entered by you");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out number);
+            Console.Clear();
+            Console.WriteLine("All Prime Numbers from 1 to "+number+" are:");
+            Prime.display(number);
+            Console.ReadKey();
         }
     }
 }
