13d169d95421f9a13bdff7a9f00fa50da9c9e8ef
Grapek <zerex93@gmail.com>
2015-04-04 22:31:51 +0200

    DecToBin and backwards, to finish

---
+++ DecToBinAndBinToDeC/DecToBinAndBinToDeC/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecToBinAndBinToDeC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to simple conversion program , from Bin to Dec and backwards");
+            String loopControl="nothing";
+            int caseSwitch = 1;
+            do{
+                Console.WriteLine("Simple Menu");
+                Console.WriteLine("1)Dec To Bin, 2)Bin To Dec, 3)Clean Screen ");
+                caseSwitch = Int32.Parse(Console.ReadLine());
+                switch (caseSwitch)
+                {
+                    case 1:
+                        translation.DecToBin();
+                        break;
+                    case 2:
+                        translation.BinToDec();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                }
+                Console.WriteLine("If you wish to exit program type exit");
+                loopControl=Console.ReadLine();
+            }while(String.Compare(loopControl,"exit",true)!=0);
+        }
+    }
+}
---
+++ DecToBinAndBinToDeC/DecToBinAndBinToDeC/translation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecToBinAndBinToDeC
+{
+    class translation
+    {
+        public static void DecToBin()
+        {
+            int DecDigit = 0; String input;
+            Console.WriteLine("Type Number you want to convert to Bin");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out DecDigit);
+            int[] array = new int[300];
+            do{
+                if (DecDigit % 2 == 0)
+                {
+
+                }
+            }while(DecDigit>0);
+            
+        }
+        public static void BinToDec()
+        {
+
+        }
+        
+    }
+}
