9f79fb2868430d5f1943742707809ce8abb20acb
Grapek <zerex93@gmail.com>
2015-04-08 22:43:22 +0200

    Celsius to kelvin

---
+++ TemperatureConverterConsole/TemperatureConverterConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature=0;
+            Console.WriteLine("Welcome To Temperature converter");
+            string input;
+            Console.WriteLine("Input Celsius");
+            input= Console.ReadLine();
+            Double.TryParse(input, out temperature);
+            Console.WriteLine(temperature);
+            
+            Console.WriteLine("Celsius degres:"+temperature);
+            Console.WriteLine("Kelvin degres:" + (temperature+273.15));
+           // temperature=(temperature-32);
+           // float temp = 5 / 9;
+           // temperature = temperature * temp;
+            //Console.WriteLine("Fahrenheit degres:" + ((temperature - 32) * (5 / 9)));
+          //  Console.WriteLine("Fahrenheit degres:" + temperature+" "+temp);
+            Console.ReadKey();
+        }
+    }
+}
